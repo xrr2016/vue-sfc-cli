7d14ed0b1a5dc3cb84a566495240f3051cf47bf0
levy <897895407@qq.com>
2019-06-03 17:56:42 +0800

    ci: -u config

--- notify.sh
+++ notify.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
-html_url=`curl https://api.github.com/repos/FEMessage/vue-sfc-cli/releases/latest | sed -n 5p | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}'`
-body=`curl https://api.github.com/repos/FEMessage/vue-sfc-cli/releases/latest | grep body | sed 's/\"body\"://g;s/\"//g'`
+if [ "$TRAVIS_TEST_RESULT" != "0" ]
+then
+  echo "build not success, bye"
+  exit 1
+fi
 
-msg='{"msgtype": "markdown", "markdown": {"title": "新版本发布", "text": "@所有人\n# ['$html_url']('$html_url')\n'$body'"}}'
+url=https://api.github.com/repos/FEMessage/vue-sfc-cli/releases/latest
+resp_tmp_file=resp.tmp
 
-#echo $msg
+curl $url > $resp_tmp_file
+
+html_url=`cat $resp_tmp_file | sed -n 5p | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}'`
+body=`cat $resp_tmp_file | grep body | sed 's/\"body\"://g;s/\"//g'`
+
+msg='{"msgtype": "markdown", "markdown": {"title": "vue-sfc-cli更新", "text": "@所有人\n# [vue-sfc-cli]('$html_url')\n'$body'"}}'
 
 curl -X POST https://oapi.dingtalk.com/robot/send\?access_token\=$DINGTALK_ROBOT_TOKEN -H 'Content-Type: application/json' -d "$msg"
 
+rm $resp_tmp_file
